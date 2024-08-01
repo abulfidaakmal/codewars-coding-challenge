23f859986a8ce63aedb1fd2fcf22f7cc181e427d
Abulfida Akmal <echo.ablfda@gmail.com>
2024-08-01 18:00:05 +0800

    Add solution for Beginner Series #1 School Paperwork

---
+++ Beginner Series #1 School Paperwork.js
@@ -0,0 +1,11 @@
+function paperwork(n, m) {
+  if (n <= 0 || m <= 0) {
+    return 0;
+  } else {
+    return n * m;
+  }
+}
+
+paperwork(5, 5);
+
+// https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd
