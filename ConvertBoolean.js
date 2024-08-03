c49684d6808246ec24fe06d803cda2df3b8222ac
Abulfida Akmal <echo.ablfda@gmail.com>
2024-08-03 20:56:06 +0800

    ConvertBoolean

---
+++ ConvertBoolean.js
@@ -0,0 +1,15 @@
+{
+    function boolToWord(bool) {
+        if (bool) {
+        return "Yes";
+        } else {
+        return "No";
+        }
+    }
+}
+
+{
+    const boolToWord = (bool) => (bool ? "Yes" : "No");
+}
+
+// https://www.codewars.com/kata/53369039d7ab3ac506000467
