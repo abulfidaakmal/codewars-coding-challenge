33cdf55b9f234f9c5273808847ee982a47926c48
Abulfida Akmal <echo.ablfda@gmail.com>
2024-08-03 20:58:04 +0800

    ConvertBoolToAString

---
+++ ConvertBoolToAString.js
@@ -0,0 +1,15 @@
+{
+    function booleanToString(b) {
+        return String(b);
+    }
+}
+
+{
+    const booleanToString = b => b.toString();
+}
+
+{
+    const booleanToString = b => `${b}`;
+}
+
+// https://www.codewars.com/kata/551b4501ac0447318f0009cd
