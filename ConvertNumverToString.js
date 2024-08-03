20ba485a33882df136e25b9984cb1a2a905bdfbc
Abulfida Akmal <echo.ablfda@gmail.com>
2024-08-03 20:59:34 +0800

    ConvertNumverToString

---
+++ ConvertNumverToString.js
@@ -0,0 +1,15 @@
+{
+    function numberToString(num) {
+        return num.toString();
+    }
+}
+
+{
+    const numberToString = num => String(num);
+}
+
+{
+    const numberToString = num => num + "";
+}
+
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8
