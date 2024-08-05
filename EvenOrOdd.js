b6c061fb8cca4cfe44aff557c032b41a737ad5e6
Abulfida Akmal <echo.ablfda@gmail.com>
2024-08-05 20:46:00 +0800

    Add solution for EvenOrOdd

---
+++ EvenOrOdd.js
@@ -0,0 +1,21 @@
+{
+    function evenOrOdd(num) {
+        if (num % 2 === 0) {
+        return "Even";
+        } else {
+        return "Odd";
+        }
+    }
+}
+
+{
+    function evenOrOdd(num) {
+        return num % 2 === 0 ? "Even" : "Odd";
+    }
+}
+
+{
+    const evenOrOdd = (num) => num % 2 === 0 ? "Even" : "Odd";
+}
+
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
