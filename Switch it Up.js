73b1def8cecfc320ce84cd4220076ffd81f2cf1c
Abulfida Akmal <echo.ablfda@gmail.com>
2024-08-18 10:51:46 +0800

    Add solution for Switch it Up

---
+++ Switch it Up.js
@@ -0,0 +1,20 @@
+function switchItUp(number) {
+  const data = [
+    "Zero",
+    "One",
+    "Two",
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight",
+    "Nine",
+  ];
+
+  return data[number];
+}
+
+switchItUp(1);
+
+// https://www.codewars.com/kata/5808dcb8f0ed42ae34000031
