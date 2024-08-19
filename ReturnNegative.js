30cad5e8e7b22e5e4f57237f8c4c1366e7263d59
Abulfida Akmal <echo.ablfda@gmail.com>
2024-08-19 11:01:40 +0800

    Add solution for ReturnNegative

---
+++ ReturnNegative.js
@@ -0,0 +1,15 @@
+{
+    function makeNegative(num) {
+        if (num >= 0) {
+        return -num;
+        } else {
+        return num;
+        }
+    }
+}
+
+{
+    const makeNegative = (num) => (num >= 0 ? -num : num);
+}
+
+// https://www.codewars.com/kata/55685cd7ad70877c23000102
