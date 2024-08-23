6ad2b321ad498a10a55209bcefbc427dbec8a909
Abulfida Akmal <echo.ablfda@gmail.com>
2024-08-23 15:11:13 +0800

    Add solution for Thinkful - Logic Drills Traffic light

---
+++ Thinkful - Logic Drills Traffic light.js
@@ -0,0 +1,11 @@
+function updateLight(current) {
+  if (current === "green") {
+    return "yellow";
+  } else if (current === "yellow") {
+    return "red";
+  } else {
+    return "green";
+  }
+}
+
+// https://www.codewars.com/kata/58649884a1659ed6cb000072
