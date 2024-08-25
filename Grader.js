4b14cc402a120373f80c83468dbdb7bab2d420d2
Abulfida Akmal <echo.ablfda@gmail.com>
2024-08-25 16:50:37 +0800

    Add solution for Grader

---
+++ Grader.js
@@ -0,0 +1,15 @@
+function grader(score) {
+  if (score > 1 || score < 0.6) {
+    return "F";
+  } else if (score >= 0.9) {
+    return "A";
+  } else if (score >= 0.8) {
+    return "B";
+  } else if (score >= 0.7) {
+    return "C";
+  } else if (score >= 0.6) {
+    return "D";
+  }
+}
+
+// https://www.codewars.com/kata/53d16bd82578b1fb5b00128c
