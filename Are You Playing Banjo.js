728c541cbed1d41c70ce2e50c4aa5b4ffbfbf099
Abulfida Akmal <echo.ablfda@gmail.com>
2024-08-25 16:49:41 +0800

    Add solution for Are You Playing Banjo

---
+++ Are You Playing Banjo.js
@@ -0,0 +1,7 @@
+function areYouPlayingBanjo(name) {
+  if (name[0].toLowerCase() === "r") return `${name} plays banjo`;
+
+  return `${name} does not play banjo`;
+}
+
+// https://www.codewars.com/kata/53af2b8861023f1d88000832
