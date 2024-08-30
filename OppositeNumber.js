01f890e8822d80c47e37d8adafdebce9657289aa
Abulfida Akmal <echo.ablfda@gmail.com>
2024-08-30 19:56:23 +0800

    Add solution for OppositeNumber

---
+++ OppositeNumber.js
@@ -0,0 +1,21 @@
+{
+    function opposite(number) {
+        if (number) {
+            return -number
+        } else {
+            return +number
+        }
+    }
+}
+
+{
+    function opposite(number) {
+        return -number
+    }
+}
+
+{
+    const opposite = number => -number
+}
+
+// https://www.codewars.com/kata/56dec885c54a926dcd001095
