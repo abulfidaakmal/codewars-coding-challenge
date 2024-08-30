863a963b1d0462c53d2e15c7e5ccd5b6d01cdd57
Abulfida Akmal <echo.ablfda@gmail.com>
2024-08-30 19:54:57 +0800

    Add solution for Simple multiplication

---
+++ Simple multiplication.js
@@ -0,0 +1,11 @@
+function simpleMultiplication(number) {
+  if (number % 2 === 0) {
+    return number * 8;
+  }
+
+  return number * 9;
+}
+
+simpleMultiplication(2);
+
+// https://www.codewars.com/kata/583710ccaa6717322c000105
