c009a3bc691413689aea63815c38d1d41be3a658
Abulfida Akmal <echo.ablfda@gmail.com>
2024-09-01 09:12:37 +0800

    Add solution for What is between

---
+++ What is between.js
@@ -0,0 +1,10 @@
+function between(a, b) {
+  const result = [];
+  for (let i = a; i <= b; i++) {
+    result.push(i);
+  }
+
+  return result;
+}
+
+// https://www.codewars.com/kata/55ecd718f46fba02e5000029
