fe066f15c2e706d8c799fa93298509d9284ef4f0
Abulfida Akmal <echo.ablfda@gmail.com>
2024-09-03 21:18:42 +0800

    Add solution for Sort array by string length

---
+++ Sort array by string length.js
@@ -0,0 +1,8 @@
+function sortByLength(array) {
+  // Return an array containing the same strings,
+  // ordered from shortest to longest
+
+  return array.sort((a, b) => a.length - b.length);
+}
+
+// https://www.codewars.com/kata/57ea5b0b75ae11d1e800006c
