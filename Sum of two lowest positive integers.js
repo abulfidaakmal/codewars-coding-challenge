3821db684d72cdc7a4d515cb4ca07a44d0a0b81e
Abulfida Akmal <echo.ablfda@gmail.com>
2024-09-21 11:37:22 +0800

    Add solution for Sum of two lowest positive integers

---
+++ Sum of two lowest positive integers.js
@@ -0,0 +1,7 @@
+function sumTwoSmallestNumbers(numbers) {
+  numbers = numbers.sort((a, b) => a - b);
+
+  return numbers[0] + numbers[1];
+}
+
+// https://www.codewars.com/kata/558fc85d8fd1938afb000014
