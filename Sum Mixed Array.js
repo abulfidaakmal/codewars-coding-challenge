301bb4b536cd4aa6e2f7651f9790d9a133098fd8
Abulfida Akmal <echo.ablfda@gmail.com>
2024-09-26 22:07:30 +0800

    Add solution for Sum Mixed Array

---
+++ Sum Mixed Array.js
@@ -0,0 +1,11 @@
+function sumMix(x) {
+  let total = 0;
+
+  for (const value of x) {
+    total += Number(value);
+  }
+
+  return total;
+}
+
+// https://www.codewars.com/kata/57eaeb9578748ff92a000009
